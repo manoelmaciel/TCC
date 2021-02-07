babb19ec190dc0fb708ce8068d20a386f3d34f96
manoelmaciel <manoelbraz.maciel@gmail.com>
2021-02-06 23:34:24 -0300

    3.1 ... O Node!

---
+++ Contacts/server.js
@@ -0,0 +1,13 @@
+// server.js
+const http = require('http');
+const host = 'localhost';
+const port = 3001;
+const server = http.createServer(function(request, response){
+response.writeHead(200, {'content-Type': 'text/html'});
+response.write('<h1>Hello ... World!</h1>');
+response.end();
+});
+
+server.listen(port, host, function(){
+console.log('Servidor ... Respondendo em: %s: %s!', host, port);
+})
