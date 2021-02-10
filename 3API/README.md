a883e78fe348ebd7c10fed723dc28deb884347cc
manoelmaciel <manoelbraz.maciel@gmail.com>
2021-02-09 23:54:58 -0300

    Concluída ... 3API!

---
+++ 3API/README.md
@@ -0,0 +1,13 @@
+# TCC
+## Trabalho de Conclusão de Curso
+
+> 3.3 A API
+
+### Para executar a aplicação:
+
+>1) $ npm install
+
+>2) $ nmp start
+
+
+
---
+++ 3API/server.js
@@ -0,0 +1,14 @@
+// server.js
+
+const http = require('http');
+const app = require('./config/express');
+
+const host = app.get('host');
+const port = app.get('port');
+
+const server = http.createServer(app);
+
+server.listen(port, host, () => {
+  	console.log('Servidor ... Respondendo em: %s: %s!', host, port);
+	}
+)
