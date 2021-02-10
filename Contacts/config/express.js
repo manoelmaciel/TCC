f525b86946f623fd2d49af595b552967e0f67fdc
manoelmaciel <manoelbraz.maciel@gmail.com>
2021-02-09 23:47:55 -0300

    3.3 API ... Concluída!

--- Contacts/config/express.js
+++ Contacts/config/express.js
@@ -1,15 +1,18 @@
 // ./config/express.js
 
-const express = require('express');
-const home = require('../app/route/contacts.js');
-const app = express();
+const express = require('express')
+const bodyParser = require('body-parser')
+const home = require('../app/route/contacts.js')
+const app = express()
 
 // enviroment variablesH
 app.set('host', 'localhost');
 app.set('port', 3003); // só muda o número da porta
 
 // middlewares
-app.use(express.static('./public'));
+app.use(express.static('./public'))
+app.use(bodyParser.urlencoded({ extended: true}))
+app.use(bodyParser.json())
 
 home(app);
 
